4cc93d0d53f982f09d9efc69c85f66a03176a796
Cereal38 <guillaume.bruncosmegazot@gmail.com>
2022-02-14 00:49:25 +0100

    Testing new push.sh

--- push.sh
+++ push.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
 git add -A
-echo "\nAdd done\n"
+echo -e "\nAdd done\n"
 
 git commit -m "$1"
-echo "Commit done\n"
+echo -e "Commit done\n"
 
 git push origin main
-echo "Push done \n"
+echo -e "Push done \n"
 
-echo
 echo "All done !"
