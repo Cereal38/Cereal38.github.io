173c492b96195bc4eaae192672802d126f824339
Cereal38 <guillaume.bruncosmegazot@gmail.com>
2022-02-23 01:10:11 +0100

    Up

--- js.00a46daa.js
+++ js.00a46daa.js
@@ -191,7 +191,24 @@ if (!on_phone) {
 }
 
 ; // END - AUTO SCROLL
-},{}],"../../../../../../AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+// SWITCH IMAGES SECTION 3 - TEST
+
+document.getElementById("section-3__box-image").innerHTML = "<p>Hello</p>";
+var image_nb = 0;
+
+document.getElementById("section-3__box-scroll-right").onclick = function () {
+  if (image_nb === 0) {
+    document.getElementById("section-3__box-image").innerHTML = "<p>To</p>";
+    image_nb = 1;
+  } else if (image_nb === 1) {
+    document.getElementById("section-3__box-image").innerHTML = "<p>You</p>";
+    image_nb = 2;
+  } else {
+    document.getElementById("section-3__box-image").innerHTML = "<p>Hello</p>";
+    image_nb = 0;
+  }
+}; // END - SWITCH IMAGES
+},{}],"../../../../AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -219,7 +236,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64813" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58865" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
@@ -395,5 +412,5 @@ function hmrAcceptRun(bundle, id) {
     return true;
   }
 }
-},{}]},{},["../../../../../../AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js","js/index.js"], null)
+},{}]},{},["../../../../AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js","js/index.js"], null)
 //# sourceMappingURL=/js.00a46daa.js.map
